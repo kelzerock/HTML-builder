7c4a8c51a8ceed376adee3f2abfc5ca03c657c20
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-05-21 23:39:03 +0300

    feat: complete task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,12 +3,11 @@ const path = require('path');
 
 const pathToStyle = `${__dirname}/styles`;
 const pathToBundle = `${__dirname}/project-dist/bundle.css`;
-const writeStream = fs.createWriteStream(pathToBundle)
+const writeStream = fs.createWriteStream(pathToBundle);
 
 
 fs.readdir(pathToStyle, (err, files)=>{
   if(err) console.log(err);
-  const arrayStyle = [];
   files.forEach(file=>{
     const fileInfo = path.parse(file);
     const fileExt = fileInfo.ext;
@@ -16,9 +15,9 @@ fs.readdir(pathToStyle, (err, files)=>{
       if(stats.isFile() && fileExt === '.css'){
         const readStream = fs.createReadStream(`${pathToStyle}/${file}`);
         readStream.on('data', (chunk) => {
-          writeStream.write(chunk)
-        })
+          writeStream.write(chunk);
+        });
       }
-    })
-  })
-})
+    });
+  });
+});
