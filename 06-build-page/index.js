b6e869457329b756039b628fdb01fe046b3ffb76
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-05-22 17:59:41 +0300

    feat: I do itgit add .!

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const pathToTemplate = path.join(__dirname, "template.html");
 const pathToDestination = path.join(__dirname, "project-dist");
 const pathToAssets = path.join(__dirname, "assets");
+const pathToAssetsDestination = path.join(__dirname, "project-dist/assets")
 const pathToComponents = path.join(__dirname, "components");
 
 async function createDir(nameDir) {
@@ -32,7 +33,56 @@ async function buildHTML() {
 
 async function createProject() {
   await createDir(pathToDestination);
+  await copyFileFunction(pathToDirectory, pathToAssetsDestination);
+  // await createMainStyle(pathToStyle, pathToBundle)
   buildHTML();
+  const writeStream = fs.createWriteStream(pathToBundle);
+
+  fs.readdir(pathToStyle, (err, files) => {
+    if (err) console.log(err);
+    files.forEach((file) => {
+      const fileInfo = path.parse(file);
+      const fileExt = fileInfo.ext;
+      fs.stat(`${pathToStyle}/${file}`, (err, stats) => {
+        if (stats.isFile() && fileExt === ".css") {
+          const readStream = fs.createReadStream(`${pathToStyle}/${file}`);
+          readStream.on("data", (chunk) => {
+            writeStream.write(chunk);
+          });
+        }
+      });
+    });
+  });
 }
 createProject();
+
+const pathToStyle = `${__dirname}/styles`;
+const pathToBundle = `${__dirname}/project-dist/style.css`;
+
+const nameToDirectory = 'assets';
+const pathToDirectory = `${__dirname}/${nameToDirectory}`;
+
+async function copyFileFunction(path, pathToDestination) {
+  if (!pathToDestination) pathToDestination = path + "-copy";
+  await fs.rm(pathToDestination, { recursive: true }, () => {
+    fs.promises.mkdir(pathToDestination, { recursive: true });
+    fs.readdir(path, (err, files) => {
+      if (err) {
+        console.log(err);
+      } else {
+        files.forEach((el) => {
+          fs.stat(`${path}/${el}`, (err, stats) => {
+            const newPath = `${path}/${el}`;
+            const newDestination = `${pathToDestination}/${el}`;
+            if (stats.isDirectory()) {
+              copyFileFunction(newPath, newDestination);
+            } else {
+              fs.promises.copyFile(newPath, newDestination);
+            }
+          });
+        });
+      }
+    });
+  });
+}
 
